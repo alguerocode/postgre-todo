114e8ae34b0297b2e9a7776c79bdd49e3fdc2715
salah <alhashmis246@gmail.com>
2021-05-31 15:48:16 +0400

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'/client/build')));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //routes 
 app.use('/todo', todoRouter);
