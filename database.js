293eefbd855d4bbc0ef6d0548ece420fbd2f128b
salah <alhashmis246@gmail.com>
2021-05-31 19:45:47 +0400

    make it better:

--- database.js
+++ database.js
@@ -3,19 +3,15 @@ require('dotenv').config();
 const pg = require('pg');
 
 
-const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  host: process.env.PG_HOST
 
-}
-const proConfig = {
-  connectionString: process.env.DATABASE_URL
-}
 
-const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig)
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+});
 
 
 
